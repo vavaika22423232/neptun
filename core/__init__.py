c86b03b8974aabab2d8e706f380b695c31d31c1d
Владимир Малик <vladimirmalik@MacBook-Air-Vladimir.local>
2025-12-23 09:12:08 +0200

    Remove incomplete modular rewrite (core/ and app_new_broken.py)

--- core/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Neptun Alarm Core Package v2.0"""
-__version__ = "2.0.0"
-
-from core.config import APP_NAME, APP_VERSION
--- core/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Core Models package
-from core.models.threat import ThreatMarker, ThreatType, Coordinates, Alarm, Device, ChatMessage, Comment
-
-__all__ = ['ThreatMarker', 'ThreatType', 'Coordinates', 'Alarm', 'Device', 'ChatMessage', 'Comment']
